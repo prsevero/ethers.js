c3dab16c9ed9ece3e10bcea7044ade3aa53e92a3
Paulo Ricardo Severo <prs.severo@gmail.com>
2023-12-23 15:24:39 -0300

    Header v1.

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="bg-sky-600 fixed flex font-bold justify-center py-4 left-0 right-0 text-xl text-white top-0 w-full">
+            <h1>ethers</h1>
+        </header>
+    )
+}
