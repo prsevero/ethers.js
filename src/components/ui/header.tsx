27115f3c327e4df02dc415b3b477fe0269767ca8
Paulo Ricardo Severo <prs.severo@gmail.com>
2023-12-28 18:47:57 -0300

    Connects to blockchain.

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,6 +1,21 @@
 export default function Header() {
     return (
-        <header className="bg-sky-600 fixed flex font-bold justify-center py-4 left-0 right-0 text-xl text-white top-0 w-full">
+        <header className={`
+            bg-sky-600
+            content-start
+            fixed
+            flex
+            font-bold
+            h-16
+            items-center
+            justify-center
+            left-0
+            right-0
+            text-xl
+            text-white
+            top-0
+            w-full
+        `}>
             <h1>ethers</h1>
         </header>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
+import { ethers } from 'ethers'
 
 import Header from '@/components/ui/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const node = 'https://frequent-delicate-friday.ethereum-sepolia.quiknode.pro/5c2b315f3c47519007ca3f988c220ea7c76ad9ca/'
+
 export default function Home() {
+  const [blockNumber, setBlockNumber] = useState(null)
+
+  async function handleConnection() {
+    const provider = new ethers.JsonRpcProvider(node)
+    const number = await provider.getBlockNumber()
+    setBlockNumber(number);
+  }
+
   return (
-    <Header />
+    <>
+      <Header />
+      <main className="flex justify-center mt-16 p-5">
+        <button
+          className={`${blockNumber ? 'bg-green-600 pointer-events-none' : 'bg-sky-900'} font-semibold px-6 py-3 rounded-lg text-white`}
+          onClick={handleConnection}
+        >
+          {blockNumber ? 'Connected' : 'Connect'} to blockchain
+        </button>
+      </main>
+    </>
   )
 }
